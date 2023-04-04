53acbd8c8999552fb77b2baca55cb4b86dee8659
SunEom <dkxl0701@gmail.com>
2023-04-04 16:36:38 +0900

    회원가입 화면 UI 수정 - 이메일 인증 버튼 추가

--- iOS/aDreamLeaf/aDreamLeaf/Presentation/SignUp/SignUpViewModel.swift
+++ iOS/aDreamLeaf/aDreamLeaf/Presentation/SignUp/SignUpViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  SignInViewModel.swift
+//  SignUpViewModel.swift
 //  aDreamLeaf
 //
 //  Created by 엄태양 on 2023/03/30.
@@ -9,6 +9,6 @@ import Foundation
 import RxSwift
 import RxRelay
 
-struct SignInViewModel {
+struct SignUpViewModel {
     let disposeBag = DisposeBag()
 }
