d3a4160a9a2c0a836eefb85b4972336aac21a408
AshRainK <kjo980430@gmail.com>
2023-04-27 21:52:51 +0900

    Backend Initialize

---
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/DreamLeafApplication.java
@@ -0,0 +1,13 @@
+package com.DreamCoder.DreamLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamLeafApplication.class, args);
+	}
+
+}
---
+++ Backend/aDreamLeaf/src/test/java/com/DreamCoder/DreamLeaf/DreamLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DreamCoder.DreamLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
