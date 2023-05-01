a80067ee2c5bab340c5769bb463130d65240b8aa
AshRainK <kjo980430@gmail.com>
2023-05-01 19:19:30 +0900

    Project Base Configuration

---
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/config/JdbcConfig.java
@@ -0,0 +1,27 @@
+package com.DreamCoder.DreamLeaf.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/adreamleaf");
+        return dataSource();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(this.dataSource());
+    }
+}
