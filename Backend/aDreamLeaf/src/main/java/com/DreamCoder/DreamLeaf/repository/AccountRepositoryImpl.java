3cd334b08fef1ac5693cdbfd1048303cda963e66
AshRainK <kjo980430@gmail.com>
2023-05-11 01:19:06 +0900

    Repository의 각 기능 테스트 및 수정

--- Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/repository/AccountRepositoryImpl.java
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/repository/AccountRepositoryImpl.java
@@ -46,7 +46,7 @@ public class AccountRepositoryImpl implements AccountRepository{
                 "','"+accountCreateDto.getUserId()+
                 "')";
         jdbcTemplate.execute(sql);
-        AccountDto accountDto = jdbcTemplate.queryForObject("SELECT * FROM account WHERE body = '"+accountCreateDto.getBody()+
+        AccountDto accountDto = jdbcTemplate.queryForObject("SELECT * FROM account WHERE accountBody = '"+accountCreateDto.getBody()+
                 "' AND created_date = '"+accountCreateDto.getDate()+"' AND userId = "+ accountCreateDto.getUserId(),accountRowMapper);
         return accountDto;
     }
@@ -62,8 +62,8 @@ public class AccountRepositoryImpl implements AccountRepository{
     @Override
     @Transactional(rollbackFor = Exception.class)
     public AccountDto update(AccountUpDto accountUpDto) { //수정 권한 확인 추가 필요 및 예외처리 필요
-        String sql = "UPDATE ACCOUNT SET restaurant = ?, price = ?, date = ?, body = ? WHERE accountId = ?";
-        jdbcTemplate.update(sql,accountUpDto.getRestaurant(),accountUpDto.getPrice(),accountUpDto.getDate(),accountUpDto.getBody(),accountUpDto.getUserId());
+        String sql = "UPDATE ACCOUNT SET restaurant = ?, price = ?, Created_date = ?, accountBody = ? WHERE accountId = ?";
+        jdbcTemplate.update(sql,accountUpDto.getRestaurant(),accountUpDto.getPrice(),accountUpDto.getDate(),accountUpDto.getBody(),accountUpDto.getAccountId());
         String sql2 = "SELECT * FROM ACCOUNT WHERE accountId = ?";
         AccountDto result = jdbcTemplate.queryForObject(sql2,accountRowMapper,accountUpDto.getAccountId());
         return result;
@@ -71,10 +71,11 @@ public class AccountRepositoryImpl implements AccountRepository{
 
     @Override
     public SimpleAccountDto inquire(int id) {
-        String sql = "SELECT amount, remain FROM ACCOUNTLOG WHERE userId = ? and createdDate = ?";
+        String sql = "SELECT amount, remain FROM ACCOUNTLOG WHERE userId = ? and createdDate BETWEEN ? AND ?";
         YearMonth currentDate = YearMonth.now();
-        String date = currentDate+"%";
-        SimpleAccountDto simpleAccountDto =  jdbcTemplate.queryForObject(sql,simpleAccountDtoRowMapper,id,date);
+        String startDate = currentDate.toString()+"-01";
+        String endDate = currentDate.toString()+"-31";
+        SimpleAccountDto simpleAccountDto =  jdbcTemplate.queryForObject(sql,simpleAccountDtoRowMapper,id,startDate,endDate);
         simpleAccountDto.setCharge(simpleAccountDto.getCharge()- simpleAccountDto.getBalance());
         return simpleAccountDto;
     }
@@ -88,19 +89,18 @@ public class AccountRepositoryImpl implements AccountRepository{
         } catch(EmptyResultDataAccessException e){
             id = -1;
         }
-        if(id != -1){
+        if(id == -1){
             String sql2 = "INSERT INTO accountsetting VALUES ("+accountSetDto.getUserId()+","+accountSetDto.getAmount()+",NOW())";
             jdbcTemplate.execute(sql2);
             String sql3 = "INSERT INTO accountlog VALUES ("+accountSetDto.getUserId()+","+accountSetDto.getAmount()+",NOW(),"+accountSetDto.getAmount()+")";
+            jdbcTemplate.execute(sql3);
         } else{
             String sql2 = "UPDATE accountsetting SET amount = ?, createdDate = NOW() WHERE userId = ?";
             jdbcTemplate.update(sql2,accountSetDto.getAmount(),accountSetDto.getUserId());
-            String sql3 = "SELECT amount,remain FROM accountlog WHERE userId = "+accountSetDto.getUserId();
             String sql4 = "UPDATE accountlog SET amount = ?, createdDate = NOW(), remain = ? WHERE userId = ?";
             int amount = jdbcTemplate.queryForObject("SELECT amount FROM accountlog WHERE userId = ?",Integer.class,accountSetDto.getUserId());
             int remain = jdbcTemplate.queryForObject("SELECT remain FROM accountlog WHERE userId = ?",Integer.class,accountSetDto.getUserId());
             remain += (accountSetDto.getAmount()-amount);
-            jdbcTemplate.update(sql3);
             jdbcTemplate.update(sql4,accountSetDto.getAmount(),remain,accountSetDto.getUserId());
         }
         return "설정이 완료 되었습니다.";
