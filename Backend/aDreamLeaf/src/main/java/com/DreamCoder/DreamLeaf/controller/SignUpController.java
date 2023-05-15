2b8dd61fb8d80fce751f230e0b359d6d3203f18d
K-Y-Hoo <bookopen1@gmail.com>
2023-05-15 19:27:47 +0900

    회원 가입 기능 4차 구현

--- Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/controller/SignUpController.java
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/controller/SignUpController.java
@@ -16,6 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 
 import java.util.Map;
 import java.util.Random;
@@ -29,16 +32,17 @@ public class SignUpController {
     private final SignUpService signUpService;
     private final AuthUtil authUtil;
 
+
     @PostMapping("/login/signUp")      // 회원가입
     public ResponseEntity createSignUp(@RequestBody SignUpCreateReq signUpCreateReq) throws FirebaseAuthException {
         String firebaseToken = signUpCreateReq.getFirebaseToken();
-        int id = authUtil.findUserId(firebaseToken);
-        String newId = Integer.toString(id);   // uid
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(firebaseToken);
+        String uid = decodedToken.getUid();
         String[] a = {"good", "brilliant", "great", "excellent", "happy"}; // 닉네임 임의 부여 조합 예시
         String[] b = {"dog", "cat", "dolphin", "racoon", "horse"};
         Random random = new Random();
         String userName = a[random.nextInt(a.length)] + " " + b[random.nextInt(b.length)];
-        SignUpCreateDto signUpCreateDto = new SignUpCreateDto(userName, signUpCreateReq.getEmail(), newId);
+        SignUpCreateDto signUpCreateDto = new SignUpCreateDto(userName, signUpCreateReq.getEmail(), uid);
         /*SignUpDto signUpDto = signUpService.create(signUpCreateDto);
         return ResponseEntity.status(201).body(signUpDto);*/
         String result = signUpService.create(signUpCreateDto);
