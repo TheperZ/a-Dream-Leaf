d1cc4de33df7138dc92dc81ac9f9ca0f5631fba7
SunEom <dkxl0701@gmail.com>
2023-05-27 00:05:31 +0900

    가게 관련 기능 - 홈 화면 주변 음식점 슬라이드

--- iOS/aDreamLeaf/aDreamLeaf/Presentation/Main/Home/HomeViewModel.swift
+++ iOS/aDreamLeaf/aDreamLeaf/Presentation/Main/Home/HomeViewModel.swift
@@ -13,4 +13,17 @@ struct HomeViewModel {
     let disposeBag = DisposeBag()
     let nearRests = Observable<[(name:String, rating:Double, distance: Double, good: Bool, card: Bool)]>.just([("브런치타임", 4.0, 130, true, true), ("조연탄", 4.3, 300, true, false), ("도라무통 즉석떡볶이", 4.1, 200, false, true)])
     
+    let nearStores = PublishSubject<[SimpleStore]>()
+    private let tempLat = 37.66346998
+    private let tempLong = 126.7641867
+    
+    init(_ storeRepo: StoreRepository = StoreRepository()) {
+        storeRepo
+            .searchNearStore(lat: tempLat, long: tempLong)
+            .map { $0.data ?? []}
+            .map { $0.count > 2 ? Array($0[...2]) : $0 }
+            .bind(to: nearStores)
+            .disposed(by: disposeBag)
+    }
+    
 }
