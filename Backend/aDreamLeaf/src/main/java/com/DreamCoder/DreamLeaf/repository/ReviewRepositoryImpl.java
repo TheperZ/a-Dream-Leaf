c59df7c847be6ae55be6c4f6d01831233432c666
changzin <changzin0126@gmail.com>
2023-05-31 22:46:29 +0900

    리뷰 사진 처리 기능 추가

--- Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/repository/ReviewRepositoryImpl.java
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/repository/ReviewRepositoryImpl.java
@@ -3,15 +3,23 @@ package com.DreamCoder.DreamLeaf.repository;
 import com.DreamCoder.DreamLeaf.dto.*;
 import com.DreamCoder.DreamLeaf.exception.ReviewException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -52,6 +60,7 @@ public class ReviewRepositoryImpl implements ReviewRepository{
                 "'," + reviewCreateDto.getRating()+
                 "," + reviewCreateDto.getUserId()+
                 ")");
+
         ReviewDto reviewDto = jdbcTemplate.queryForObject("SELECT * FROM REVIEW WHERE body = '"+reviewCreateDto.getBody()+
                 "' AND created_date = '"+reviewCreateDto.getDate()+
                 "' AND storeId = "+reviewCreateDto.getStoreId()+
@@ -59,6 +68,9 @@ public class ReviewRepositoryImpl implements ReviewRepository{
 
         reviewDto.setNameData(userName, storeName);
 
+        if (reviewCreateDto.getReviewImage() != null && !reviewCreateDto.getReviewImage().isEmpty()) {
+            saveReviewImage(reviewDto.getDate(), reviewDto.getStoreId(), reviewDto.getReviewId(), reviewCreateDto.getReviewImage());
+        }
         return reviewDto;
     }
 
@@ -106,6 +118,18 @@ public class ReviewRepositoryImpl implements ReviewRepository{
                 "', rating="+reviewUpDto.getRating()+
                 " WHERE reviewId="+reviewUpDto.getReviewId()
                 );
+
+        if (reviewUpDto.getReviewImage() == null || reviewUpDto.getReviewImage().isEmpty()) {
+            delReviewImage(reviewUpDto.getReviewId());
+        }
+        else{
+            if (countReviewImage(reviewUpDto.getReviewId()) == 0){
+                saveReviewImage(reviewUpDto.getDate(), getStoreId(reviewUpDto.getReviewId()), reviewUpDto.getReviewId(), reviewUpDto.getReviewImage());
+            }
+            else{
+                updateReviewImage(reviewUpDto.getDate(), getStoreId(reviewUpDto.getReviewId()), reviewUpDto.getReviewId(), reviewUpDto.getReviewImage());
+            }
+        }
         return "수정이 완료 되었습니다.";
     }
 
@@ -117,9 +141,127 @@ public class ReviewRepositoryImpl implements ReviewRepository{
         if (writerId != reviewDelDto.getUserId()){
             throw new ReviewException("삭제 권한이 없습니다.", 403);
         }
+        if (countReviewImage(reviewDelDto.getReviewId()) != 0){
+            delReviewImage(reviewDelDto.getReviewId());
+        }
 
-        jdbcTemplate.update("DELETE FROM REVIEW WHERE reviewId="+reviewDelDto.getReviewId());
+        try{
+            jdbcTemplate.update("DELETE FROM REVIEW WHERE reviewId="+reviewDelDto.getReviewId());
+        }
+        catch(Exception e){
+            throw new ReviewException("삭제할 리뷰를 찾을 수 없습니다.", 404);
+        }
         return "삭제가 완료 되었습니다.";
+    }
+
+    @Override
+    public ReviewImageDto getReviewImage(int reviewId) {
+        String imageUrl;
+        String imageTitle;
+        UrlResource urlResource;
+        String sql = "SELECT imageUrl FROM reviewimage WHERE reviewId = ?";
+        String sql2 = "SELECT imageTitle FROM reviewimage WHERE reviewId = ?";
+
+        String rootPath;
+        try{
+            rootPath = (Paths.get("").toRealPath() + "\\src\\main\\resources").replace('\\', '/');
+        }
+        catch (IOException e){
+            throw new ReviewException("이미지를 저장한 경로를 찾을 수 없습니다.", 404);
+        }
+
+        try{
+            imageUrl = jdbcTemplate.queryForObject(sql, String.class, reviewId);
+            imageTitle = jdbcTemplate.queryForObject(sql2, String.class, reviewId);
+            urlResource = new UrlResource("file:" + Paths.get(rootPath + imageUrl));
+        }
+        catch (Exception ex){
+            throw new ReviewException("리뷰 이미지를 찾을 수 없습니다.", 404);
+        }
+        ReviewImageDto reviewImageDto = new ReviewImageDto(imageTitle, imageUrl, urlResource);
+        return reviewImageDto;
+    }
+
+    @Override
+    public void saveReviewImage(String date, int storeId, int reviewId, MultipartFile reviewImage) {
+        int idx = reviewImage.getOriginalFilename().lastIndexOf(".");
+        String ext = reviewImage.getOriginalFilename().substring(idx);
+
+        String rootPath;
+        try{
+            rootPath = (Paths.get("").toRealPath() + "\\src\\main\\resources").replace('\\', '/');
+        }
+        catch (IOException e){
+            throw new ReviewException("이미지를 저장할 경로를 찾을 수 없습니다.", 404);
+        }
+
+        String imageTitle = date.substring(0, 10) + "-" + storeId + "-" + reviewId + ext;
+        String imageUrl = validateImageUrl(rootPath, date) + "/" + imageTitle;
+
+        try {
+            reviewImage.transferTo(Paths.get(rootPath + imageUrl));
+        }
+        catch (Exception e){
+            throw new ReviewException("이미지 파일을 찾을 수 없습니다.", 404);
+        }
+        jdbcTemplate.execute("INSERT INTO reviewimage(imageTitle, imageUrl, reviewId) VALUES('" +
+                imageTitle +
+                "','" + imageUrl +
+                "'," + reviewId +
+                ")");
+    }
+
+    @Override
+    public void updateReviewImage(String date, int storeId, int reviewId, MultipartFile reviewImage) {
+        String rootPath;
+        try{
+            rootPath = (Paths.get("").toRealPath() + "\\src\\main\\resources").replace('\\', '/');
+        }
+        catch (IOException e){
+            throw new ReviewException("이미지를 저장할 경로를 찾을 수 없습니다.", 404);
+        }
+
+        try{
+            String selectSql = "SELECT imageUrl FROM reviewimage WHERE reviewId = ?";
+            String imageUrl = jdbcTemplate.queryForObject(selectSql, String.class, reviewId);
+            if (imageUrl == null){
+                throw new ReviewException("이미지를 저장할 경로를 찾을 수 없습니다.", 404);
+            }
+            Files.delete(Paths.get(rootPath + imageUrl));
+        }
+        catch (Exception ex){
+            throw new ReviewException("삭제할 이미지를 찾을 수 없습니다.", 404);
+        }
+
+        int idx = reviewImage.getOriginalFilename().lastIndexOf(".");
+        String ext = reviewImage.getOriginalFilename().substring(idx);
+
+        String imageTitle = date.substring(0, 10) + "-" + storeId + "-" + reviewId + ext;
+        String imageUrl = validateImageUrl(rootPath, date) + "/" + imageTitle;
+        String updateSql = "UPDATE reviewimage SET imageUrl = ?, imageTitle = ? WHERE reviewId = ?";
+
+        try {
+            reviewImage.transferTo(Paths.get(rootPath + imageUrl));
+            jdbcTemplate.update(updateSql, imageUrl, imageTitle, reviewId);
+        }
+        catch (Exception e){
+            throw new ReviewException("이미지 파일을 찾을 수 없습니다.", 404);
+        }
+    }
+
+    @Override
+    public void delReviewImage(int reviewId) {
+        String selectSql = "SELECT imageUrl FROM reviewimage WHERE reviewId = ?";
+        String deleteSql = "DELETE FROM reviewimage WHERE reviewId = ?";
+        String imageUrl = jdbcTemplate.queryForObject(selectSql, String.class, reviewId);
+        try{
+            String rootPath = (Paths.get("").toRealPath() + "\\src\\main\\resources").replace('\\', '/');
+            Files.delete(Paths.get(rootPath + imageUrl));
+            jdbcTemplate.update(deleteSql, reviewId);
+        }
+        catch (Exception e){
+            throw new ReviewException("이미지를 저장할 경로를 찾을 수 없습니다.", 404);
+        }
     }
 
     @Override
@@ -174,5 +316,45 @@ public class ReviewRepositoryImpl implements ReviewRepository{
             throw new ReviewException("조회하려는 리뷰가 존재하지 않습니다.", 404);
         }
         return writerId;
+    }
+
+    @Override
+    public String validateImageUrl(String rootPath, String date) {
+        String imageUrl;
+
+        try {
+            File folder = new File(rootPath + "/reviewImage");
+            if (!folder.exists()){
+                folder.mkdir();
+            }
+            imageUrl = "/reviewImage/" + date.substring(0, 7);
+            folder = new File(rootPath + imageUrl);
+            if (!folder.exists()){
+                folder.mkdir();
+            }
+        }
+        catch (Exception e){
+            throw new ReviewException("이미지를 저장할 경로를 찾을 수 없습니다.", 404);
+        }
+        return imageUrl;
+    }
+
+    @Override
+    public int countReviewImage(int reviewId) {
+        int count = jdbcTemplate.queryForObject("SELECT count(*) FROM reviewimage WHERE reviewId="+reviewId, Integer.class);
+        return count;
+    }
+
+    @Override
+    public int getStoreId(int reviewId) {
+        String sql = "SELECT storeId FROM review WHERE reviewId = ?";
+        int storeId;
+        try{
+            storeId = jdbcTemplate.queryForObject(sql, Integer.class, reviewId);
+        }
+        catch (EmptyResultDataAccessException ex){
+            throw new ReviewException("조회하려는 리뷰가 존재하지 않습니다.", 404);
+        }
+        return storeId;
     }
 }
--- Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/service/ReviewService.java
+++ Backend/aDreamLeaf/src/main/java/com/DreamCoder/DreamLeaf/service/ReviewService.java
@@ -6,8 +6,15 @@ import com.DreamCoder.DreamLeaf.req.ReviewDelReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -16,20 +23,15 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepositoryImpl;
 
-    public ReviewDto create(ReviewCreateDto reviewCreateDto){
-        return reviewRepositoryImpl.save(reviewCreateDto);
-    }
+    public ReviewDto create(ReviewCreateDto reviewCreateDto) { return reviewRepositoryImpl.save(reviewCreateDto); }
 
-    public List<ReviewDto> findReviewPage(ReviewSearchDto reviewSearchDto){
+    public List<ReviewDto> findReviewPage(ReviewSearchDto reviewSearchDto) { return reviewRepositoryImpl.findReviewPage(reviewSearchDto);}
 
-        return reviewRepositoryImpl.findReviewPage(reviewSearchDto);
-    }
+    public String update(ReviewUpDto reviewUpDto) {return reviewRepositoryImpl.update(reviewUpDto);}
 
-    public String update(ReviewUpDto reviewUpDto) { return reviewRepositoryImpl.update(reviewUpDto);}
-
-    public String delete(ReviewDelDto reviewDelDto){
+    public String delete(ReviewDelDto reviewDelDto) {
         return reviewRepositoryImpl.delete(reviewDelDto);
     }
 
-
+    public ReviewImageDto getReviewImage(int reviewId) { return reviewRepositoryImpl.getReviewImage(reviewId); }
 }
