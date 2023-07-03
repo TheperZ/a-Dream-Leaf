f8ef1a0d6afaed46590d013a1d0e906169487352
SunEom <dkxl0701@gmail.com>
2023-07-03 13:06:05 +0900

    계정 관련 기능 - 계정 탈퇴 기능 구현

--- iOS/aDreamLeaf/aDreamLeaf/Repository/ProfileRepository.swift
+++ iOS/aDreamLeaf/aDreamLeaf/Repository/ProfileRepository.swift
@@ -12,12 +12,22 @@ struct ProfileRepository {
     private let network = ProfileNetwork()
     
     func deleteAccount() -> Observable<RequestResult<Void>> {
-        return network.deleteAccountFB()
-            .map { result in
-                if result.success {
-                    UserManager.logout()
+        
+        //Server -> FB 순으로 삭제 요청 : Firebase Uid가 필요하기 때문...
+        return network.deleteAccountServer() // 서버측에 정보 삭제 요청
+            .flatMap { result in
+                if result.success {// 서버측 정보 삭제 성공
+                    return network.deleteAccountFB()
+                        .map { result in
+                            if result.success {// Firebase 계정 삭제에 성공
+                                UserManager.logout() // Firebase 계정 삭제 성공시 로그아웃 처리
+                            }
+                            
+                            return result
+                        }
+                } else { // 서버측에 정보 삭제 요청 실패
+                    return Observable.just(result)
                 }
-                return result
             }
     }
 }
