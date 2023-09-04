3eb4faf012d731bcc97e83f0bfc82e062ee41149
SunEom <dkxl0701@gmail.com>
2023-09-04 17:01:18 +0900

    카카오 API  관련 기능 - 리펙토링 과정 오류 수정

--- iOS/aDreamLeaf/aDreamLeaf/Repository/KakaoRepository.swift
+++ iOS/aDreamLeaf/aDreamLeaf/Repository/KakaoRepository.swift
@@ -9,25 +9,15 @@ import Foundation
 import RxSwift
 
 struct KakaoRepositroy {
-    private let network = KakaoNetwork(type: .Kakao)
+    private let network = KakaoNetwork()
     
     func getAddressKakao(lat: Double, lon: Double) -> Observable<String>{
         return network.getAddress(lat: lat, lon: lon)
             .map { result in
-                if result.success { // 통신 성공
-                    if let data = result.data, data.count > 0 {
-                        if data[0].road_address != nil {
-                            return data[0].road_address!.address_name
-                        } else if data[0].address != nil {
-                            return data[0].address!.address_name
-                        } else {
-                            return "주소를 찾을 수 없습니다!"
-                        }
-                    } else {
-                        return "주소를 찾을 수 없습니다!"
-                    }
+                if result != nil {
+                    return result![0].address?.address_name ?? result![0].road_address?.address_name ?? "위치를 찾을 수 없습니다."
                 } else {
-                    return result.msg!
+                    return "오류가 발생했습니다. 잠시후 시도해주세요."
                 }
             }
     }
