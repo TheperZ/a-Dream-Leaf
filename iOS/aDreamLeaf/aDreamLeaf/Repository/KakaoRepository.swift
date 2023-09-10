5fea67ca9c407a96d79962ac7d5d7bd40763497e
SunEom <dkxl0701@gmail.com>
2023-09-10 17:15:28 +0900

    주변 가게 관련 기능 - 카카오 주소 API 처리 수정

--- iOS/aDreamLeaf/aDreamLeaf/Repository/KakaoRepository.swift
+++ iOS/aDreamLeaf/aDreamLeaf/Repository/KakaoRepository.swift
@@ -11,14 +11,18 @@ import RxSwift
 struct KakaoRepositroy {
     private let network = KakaoNetwork()
     
-    func getAddressKakao(lat: Double, lon: Double) -> Observable<String>{
-        return network.getAddress(lat: lat, lon: lon)
-            .map { result in
+    func getMyAddress() -> Observable<String> {
+        if LocationManager.permitionCheck() {
+            return network.getAddress(lat: LocationManager.getLatitude()!, lon: LocationManager.getLongitude()!).map { result in
                 if result != nil {
                     return result![0].address?.address_name ?? result![0].road_address?.address_name ?? "위치를 찾을 수 없습니다."
                 } else {
                     return "오류가 발생했습니다. 잠시후 시도해주세요."
                 }
             }
+            
+        } else {
+            return Observable.just("위치 정보 제공 동의를 해주세요")
+        }
     }
 }
